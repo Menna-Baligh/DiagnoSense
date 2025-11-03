0ff6152e851f371158ae2c088d7116157e1c120a
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-11-03 15:12:10 +0200

    implement google social authentication with Socialite integration

---
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Testing\Fluent\Concerns\Has;
+use Laravel\Socialite\Socialite;
+
+class SocialAuthController extends Controller
+{
+    public function redirectToGoogle() {
+        return response()->json([
+            'url' => Socialite::driver('google')->stateless()->redirect()->getTargetUrl(),
+        ]);
+    }
+
+    public function handleGoogleCallback() {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+
+        $user = User::updateOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name' => $googleUser->getName(),
+                'password' => Hash::make(Str::random(16)),
+                'provider' => 'google',
+                'provider_id' => $googleUser->getId(),
+            ]
+        );
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User successfully logged in',
+            'data' => [
+                'user' => [
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'created_at' => $user->created_at,
+                    'updated_at' => $user->updated_at,
+                    'id' => $user->id
+                ],
+                'token' => $token,
+            ],
+        ]);
+    }
+}
