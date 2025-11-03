7248b46f87a16a73b79f798749c2eb959b4d07cf
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-11-03 19:29:37 +0200

    Add ForceJsonResponse middleware to enforce JSON response format

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->group('api', [
+            \App\Http\Middleware\ForceJsonResponse::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        
+
     })->create();
