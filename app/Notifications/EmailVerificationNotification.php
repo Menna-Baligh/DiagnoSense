5f041b79e685d27dc7ed385f6f80b7603dbc2d55
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-11-04 19:46:46 +0200

    Remove ShouldQueue interface from notification classes

--- app/Notifications/EmailVerificationNotification.php
+++ app/Notifications/EmailVerificationNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class EmailVerificationNotification extends Notification implements ShouldQueue
+class EmailVerificationNotification extends Notification 
 {
     use Queueable;
     private $otp;
--- app/Notifications/RegisterNotification.php
+++ app/Notifications/RegisterNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class RegisterNotification extends Notification implements ShouldQueue
+class RegisterNotification extends Notification 
 {
     use Queueable;
 
