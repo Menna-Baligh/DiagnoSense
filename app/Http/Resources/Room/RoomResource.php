df705a3f4a1e57676e19d07231fc99ac9cb49bef
Abdelrahman Hassan <152273218+Abdelrahman5884@users.noreply.github.com>
2025-11-25 23:59:34 +0200

    edit creation room

---
+++ app/Http/Resources/Room/RoomResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Room;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RoomResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id'         => $this->id,
+            'room-code'       => $this->room_code,
+            'title'      => $this->title,
+            'patient_id' => $this->patient_id,
+            'doctor_id'  => $this->doctor_id,
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+        ];
+    }
+}
