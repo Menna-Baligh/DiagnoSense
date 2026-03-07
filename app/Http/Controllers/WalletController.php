63401cedca783c77dbe47f0a39e2feac86bca9f2
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-03-07 09:19:50 +0200

    add WallController with chagre logic, ChargeWalletRequest validation

---
+++ app/Http/Controllers/WalletController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ChargeWalletRequest;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
+
+class WalletController extends Controller
+{
+    public function charge(ChargeWalletRequest $request) {
+        Stripe::setApiKey(config('services.stripe.secret'));
+
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'egp',
+                    'product_data' => [
+                        'name' => 'Wallet Charge',
+                    ],
+                    'unit_amount' => $request->balance * 100
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'client_reference_id' => auth()->user()->doctor->id,
+            'metadata' => [
+                'doctor_id'  => auth()->user()->doctor->id,
+                'amount' => $request->balance,
+            ],
+            'success_url' => route('payment.success'),
+            'cancel_url' => route('payment.cancel'),
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'checkout_url' => $session->url,
+        ]);
+    }
+}
