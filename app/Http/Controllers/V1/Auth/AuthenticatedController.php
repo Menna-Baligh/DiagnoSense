6780312c0898b4d844f5c802c7956acc1194e0e9
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-05-24 21:09:52 +0300

    Refactor: move `LogoutRequest` to `Auth` namespace and update references in `AuthenticatedController`

--- app/Http/Controllers/V1/Auth/AuthenticatedController.php
+++ app/Http/Controllers/V1/Auth/AuthenticatedController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\V1\Auth;
 
 use App\Helpers\ApiResponse;
 use App\Http\Requests\Auth\LoginRequest;
-use App\Http\Requests\LogoutRequest;
+use App\Http\Requests\Auth\LogoutRequest;
 use App\Http\Resources\UserResource;
 use App\Services\Auth\AuthenticationService;
 use Illuminate\Http\JsonResponse;
