f4c1255e3043b258f473828f2196b29921556c70
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-05-24 21:56:11 +0300

    Remove unused `register` method from `EventServiceProvider`

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,14 +17,6 @@ class EventServiceProvider extends ServiceProvider
     ];
 
     /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
      * Bootstrap services.
      */
     public function boot(): void
