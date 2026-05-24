06d8d0af8730f02250442b0bba57cba357deb901
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-05-24 21:40:47 +0300

    Type-hint `subscriptions()` method in `Plan` model with `HasMany`

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Stripe\Subscription;
 
 class Plan extends Model
@@ -24,7 +25,7 @@ class Plan extends Model
         'features' => 'array',
     ];
 
-    public function subscriptions()
+    public function subscriptions(): HasMany
     {
         return $this->hasMany(Subscription::class);
     }
