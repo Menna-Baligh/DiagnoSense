ad22a9bbc0beaeca5aa31b7af1e40d474b1e0dcc
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-05-26 00:45:19 +0300

    Add user type authorization to `ResetPasswordRequest`

--- app/Http/Requests/Auth/ResetPasswordRequest.php
+++ app/Http/Requests/Auth/ResetPasswordRequest.php
@@ -6,6 +6,12 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ResetPasswordRequest extends FormRequest
 {
+    public function authorize(): bool
+    {
+        $user = auth()->user();
+        $type = $this->route('type');
+        return $user->type === $type;
+    }
     public function rules(): array
     {
         return [
